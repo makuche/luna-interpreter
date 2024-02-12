1faba2035db277aa3831602a7f2629c83fa30f4e
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2024-02-12 17:26:00 +0100

    feat: array to hold line number for debugger

--- clox/chunk.h
+++ clox/chunk.h
@@ -2,19 +2,28 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
+/* Each chunk has a one byte operation code.
+This number controls the kind of instruction. */
 typedef enum {
-    OP_RETURN,
+    OP_CONSTANT,
+    OP_RETURN,  // return from current function
 } OpCode;
 
+/* Struct to store along some data with a instruction,
+currently this is just a series of bytes. */
 typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
